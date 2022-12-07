4bf5f7ca8f858624adf731a21a7ee018884bfe95
xkjiang-srfv <52552899+xkjiang-srfv@users.noreply.github.com>
2022-12-07 19:09:48 +0800

    Add files via upload

---
+++ HighBit/model.py
@@ -0,0 +1,412 @@
+from collections import OrderedDict
+import torch.nn as nn
+import torch.utils.model_zoo as model_zoo
+import torch.nn.functional as F
+import math
+import torch
+class ThuVggNet(nn.Module):
+    def __init__(self, features,classifier=None):
+        super(ThuVggNet, self).__init__()
+        self.features = features
+        if classifier:
+            self.classifier = classifier
+        else:
+            self.classifier = nn.Sequential(
+                nn.Linear(512 * 7 * 7, 4096),
+                nn.ReLU(inplace=True),
+                nn.Dropout(),
+                nn.Linear(4096, 4096),
+                nn.ReLU(inplace=True),
+                nn.Dropout(),
+                nn.Linear(4096, 10),# cifar10
+            )
+
+    def forward(self, x):
+        if hasattr(self,"first_input_prune"):
+            x = self.first_input_prune(x)
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+def thu_vgg_make_classifer(cfg):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers.append(nn.MaxPool2d(kernel_size=2,stride=2))
+        elif v=='Special':
+            layers.append(nn.Conv2d(in_channels,1024,kernel_size=3,padding=0))
+            v=1024
+        else:
+            layers.append(nn.Conv2d(in_channels,v,kernel_size=3,padding=1))
+            layers.append(nn.BatchNorm2d(v))
+            layers.append(nn.ReLU(inplace=True))
+            in_channels=v
+    return nn.Sequential(*layers)
+
+class ZFNet(nn.Module):
+    def __init__(self, num_classes=10):
+        super(ZFNet, self).__init__()
+        self.features = nn.Sequential(  # 打包
+            nn.Conv2d(3, 48, kernel_size=7, stride=2, padding=1),   # input[3, 224, 224]  output[48, 110, 110] 自动舍去小数点后
+            nn.BatchNorm2d(48),
+            nn.ReLU(inplace=True),  # inplace 可以载入更大模型
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),       # output[48, 55, 55] kernel_num为原论文一半
+            nn.Conv2d(48, 128, kernel_size=5, stride=2),            # output[128, 26, 26]
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),       # output[128, 13, 13]
+            nn.Conv2d(128, 192, kernel_size=3, padding=1),          # output[192, 13, 13]
+            nn.BatchNorm2d(192),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(192, 192, kernel_size=3, padding=1),          # output[192, 13, 13]
+            nn.BatchNorm2d(192),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(192, 128, kernel_size=3, padding=1),          # output[128, 13, 13]
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),                  # output[128, 6, 6]
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.5),
+            # 全连接
+            nn.Linear(128 * 6 * 6, 2048),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(2048, 2048),
+            nn.ReLU(inplace=True),
+            nn.Linear(2048, num_classes),
+        )
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, start_dim=1)  # 展平   或者view()
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')  # 何教授方法
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)  # 正态分布赋值
+                nn.init.constant_(m.bias, 0)
+
+class AlexNet(nn.Module):
+
+    def __init__(self, num_classes=10):
+        super(AlexNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=2),
+            nn.BatchNorm2d(96),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(96, 256, kernel_size=5, padding=2),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(256, 384, kernel_size=3, padding=1),
+            nn.BatchNorm2d(384),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 384, kernel_size=3, padding=1),
+            nn.BatchNorm2d(384),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            # nn.BatchNorm1d(4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            # nn.BatchNorm1d(4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+            # nn.Softmax()
+        )
+
+    def forward(self, x):
+        if hasattr(self, "first_input_prune"):
+            x = self.first_input_prune(x)
+        x = self.features(x)
+        x = x.view(x.size(0), 256 * 6 * 6)
+        x = self.classifier(x)
+        return x
+class LeNet(nn.Module):
+    def __init__(self, num_classes=10):
+        super(LeNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 6, kernel_size=5),
+            nn.BatchNorm2d(6),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(6, 16, kernel_size=5),
+            nn.BatchNorm2d(16),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(16, 120, kernel_size=5),
+            # nn.BatchNorm2d(120),
+            nn.ReLU(inplace=True)
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(120, 84),
+            nn.BatchNorm1d(84),
+            nn.ReLU(inplace=True),
+            nn.Linear(84, num_classes)
+        )
+
+    def forward(self, x):
+        if hasattr(self, "first_input_prune"):
+            x = self.first_input_prune(x)
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+# class VGG(nn.Module):
+#
+#     def __init__(self, features, num_classes=10):
+#         super(VGG, self).__init__()
+#         self.features = features
+#         self.classifier = nn.Sequential(
+#             nn.Linear(512 * 7 * 7, 4096),
+#             nn.ReLU(inplace=True),
+#             nn.Dropout(),
+#             nn.Linear(4096, 4096),
+#             nn.ReLU(inplace=True),
+#             nn.Dropout(),
+#             nn.Linear(4096, num_classes),
+#         )
+#         self._initialize_weights()
+#
+#     def forward(self, x):
+#         if hasattr(self, "first_input_prune"):
+#             x = self.first_input_prune(x)
+#         x = self.features(x)
+#         x = x.view(x.size(0), -1)
+#         x = self.classifier(x)
+#         return x
+#
+#     def _initialize_weights(self):
+#         for m in self.modules():
+#             if isinstance(m, nn.Conv2d):
+#                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+#                 m.weight.data.normal_(0, math.sqrt(2. / n))
+#                 if m.bias is not None:
+#                     m.bias.data.zero_()
+#             elif isinstance(m, nn.BatchNorm2d):
+#                 m.weight.data.fill_(1)
+#                 m.bias.data.zero_()
+#             elif isinstance(m, nn.Linear):
+#                 n = m.weight.size(1)
+#                 m.weight.data.normal_(0, 0.01)
+#                 m.bias.data.zero_()
+
+class ResNet(nn.Module):
+    def __init__(self, block, layers, num_classes=10):
+        self.inplanes = 64
+        super(ResNet, self).__init__()
+
+        m = OrderedDict()
+        m['conv1'] = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        m['bn1'] = nn.BatchNorm2d(64)
+        m['relu1'] = nn.ReLU(inplace=True)
+        m['maxpool'] = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.group1 = nn.Sequential(m)
+
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.Sequential(nn.AvgPool2d(7))
+        self.group2 = nn.Sequential(
+            OrderedDict([
+                ('fc', nn.Linear(512 * block.expansion, num_classes))
+            ])
+        )
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.group1(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.group2(x)
+
+        return x
+class NewResNet(nn.Module):
+    def __init__(self, block, layers, num_classes=10):
+        self.inplanes = 64
+        super(NewResNet, self).__init__()
+
+        m = OrderedDict()
+        m['conv1'] = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        m['bn1'] = nn.BatchNorm2d(64)
+        m['relu1'] = nn.ReLU(inplace=True)
+        self.group1 = nn.Sequential(m)
+
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.maxpool = nn.Sequential(nn.MaxPool2d(kernel_size=3, stride=2))
+        self.group2 = nn.Sequential(
+            OrderedDict([
+                ('fc', nn.Linear(512 * block.expansion, num_classes))
+            ])
+        )
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.group1(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.maxpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.group2(x)
+
+        return x
+class ResNetBasicBlock(nn.Module):
+    expansion = 1
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(ResNetBasicBlock, self).__init__()
+        self.downsample = downsample
+        m = OrderedDict()
+        m['conv1'] = nn.Conv2d(inplanes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        m['relu1'] = nn.ReLU(inplace=True)
+        m['conv2'] = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.group1 = nn.Sequential(m)
+        self.relu = nn.Sequential(nn.ReLU(inplace=True))
+
+
+    def forward(self, x):
+        if self.downsample is not None:
+            residual = self.downsample(x)
+        else:
+            residual = x
+        out = residual + self.group1(x)
+        out = self.relu(out)
+        return out
+class VGG8(nn.Module):
+    def __init__(self, num_classes=10):
+        super(VGG8, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(512, 1024, kernel_size=3),
+            nn.BatchNorm2d(1024),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(1024, num_classes),
+            # nn.Softmax()
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+
+
+def get_thu_vgg16(num_classes=10):
+    vgg16_setting = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
+    fc_layers = nn.Sequential(
+        nn.Dropout(),
+        nn.Linear(512, num_classes)
+    )
+    model = ThuVggNet(thu_vgg_make_classifer(vgg16_setting), fc_layers)
+    return model
+def getLeNet(num_classes=10):
+    model = LeNet(num_classes)
+    return model
+def getAlexnet(num_classes=10):
+    model = AlexNet(num_classes)
+    return model
+def get_resnet18(num_classes=10):
+    model = ResNet(ResNetBasicBlock, [2, 2, 2, 2], num_classes)
+    return model
+def get_newRsenet18(num_classes = 10):
+    model = NewResNet(ResNetBasicBlock, [2, 2, 2, 2], num_classes)
+    return model
+def getVgg8(num_classes=10):
+    model = VGG8(num_classes)
+    return model
+def get_ZFNet(num_class=10):
+    model = ZFNet(num_class)
+    return model
